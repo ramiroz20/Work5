83323e4c94a9fddb61b49e20bf573a087cdb0e01
Roman <romantuser69@gmail.com>
2020-07-01 16:08:03 +0300

    Initial commit

---
+++ src/com/company/Work5.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Work5 {
+    public static void main(String args[]) {
+        // объявляем и инициализируем три переменные
+        int a = -1;
+
+        if (a < 0) {      // если a меньше 0
+            System.out.println("число отрицательное");
+        }
+        if (a == 0) {    // если a равно 0
+            System.out.println("число положительное");
+        }
+
+
+        if (a > 0) {
+            System.out.println("число положительное");
+        }
+    }
+
+}
